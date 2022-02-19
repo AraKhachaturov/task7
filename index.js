22345150116e07a7dc3835ba65bfc4e13b19c71f
Ara <g12.Khachaturov.ara@gmail.com>
2022-02-20 01:02:11 +0400

    almost Ready

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-ECHO is on.
+let paths =__dirname;
+let fs= require("fs");
+let util=require("util");
+let imputedDirectory=process.argv[2];
+//console.log(util);
+
+
+fs.readdir(imputedDirectory, function(err,file){
+
+file.forEach(function(item,index){
+
+let fullPath=imputedDirectory+"/"+item;
+console.log(item);
+    fs.stat(fullPath,(err,fileStat)=>{
+console.log(fileStat);
+
+    });
+});
+
+});
+
+
+console.log("well done");
+console.log(process.argv[2]);
+console.log(paths);
+console.log(process.cwd());
