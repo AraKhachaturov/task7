1e14347b684e92f834d5d20cf74f313c10c6df94
Ara <g12.Khachaturov.ara@gmail.com>
2022-02-20 01:18:29 +0400

    beautified

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-let paths =__dirname;
-let fs= require("fs");
-let util=require("util");
-let imputedDirectory=process.argv[2];
+let paths = __dirname;
+let fs = require("fs");
+let util = require("util");
+let imputedDirectory = process.argv[2];
 //console.log(util);
 
-
-fs.readdir(imputedDirectory, function(err,file){
-
-file.forEach(function(item,index){
-
-let fullPath=imputedDirectory+"/"+item;
-console.log(item);
-    fs.stat(fullPath,(err,fileStat)=>{
-console.log(fileStat);
-
+fs.readdir(imputedDirectory, function (err, file) {
+  file.forEach(function (item, index) {
+    let fullPath = imputedDirectory + "/" + item;
+    console.log(item);
+    fs.stat(fullPath, (err, fileStat) => {
+      console.log(fileStat.size);
     });
+  });
 });
-
-});
-
 
 console.log("well done");
 console.log(process.argv[2]);
