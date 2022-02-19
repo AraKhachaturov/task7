25141ff60456f444e77ddc801d14deb64434d81d
Ara <g12.Khachaturov.ara@gmail.com>
2022-02-20 03:13:10 +0400

    second Vertion

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-let paths = __dirname;
-let fs = require("fs");
-let util = require("util");
-let imputedDirectory = process.argv[2];
-//console.log(util);
+let fs = require('fs');
+let path = require('path');
 
-fs.readdir(imputedDirectory, function (err, file) {
-  file.forEach(function (item, index) {
-    let fullPath = imputedDirectory + "/" + item;
-    console.log(item);
-    fs.stat(fullPath, (err, fileStat) => {
-      let fileSize = fileStat.size;
-      fs.appendFile("sorted_files.txt", `${fullPath}-----${fileSize }\n`,(err)=>{
-          console.log(err);
+
+let walk = function(dir, done) {
+  let results = [];
+  fs.readdir(dir, function(err, list) {
+    if (err) return done(err);
+    let pending = list.length;
+    if (!pending) return done(null, results);
+    list.forEach(function(file) {
+
+
+      file = path.resolve(dir, file);
+      console.log(file)
+      fs.stat(file, function(err, stat) {
+        if (stat && stat.isDirectory()) {
+          walk(file, function(err, res) {
+            results = results.concat(res);
+            if (!--pending) done(null, results);
+          });
+        } else {
+          results.push({path:path.relative( process.cwd(),file), size: stat.size});
+          if (!--pending) done(null, results);
+        }
       });
     });
   });
-});
+};
 
-console.log("well done");
-console.log(process.argv[2]);
-console.log(paths);
-console.log(process.cwd());
+
+walk(process.argv[2], function(err, results) {
+  if (err) throw err;
+    
+    let resultToSave=results.sort((i1,i2)=>{
+        return i2.size -i1.size;
+    }).map(item =>{
+            return `${item.path.split(path.sep).join("/")}------------${item.size}`;
+    }).join('\n');
+
+    console.log(results);
+    
+    console.log(resultToSave);
+
+
+  });
